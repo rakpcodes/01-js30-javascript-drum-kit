1e2821764fead4da6c216549649c1103b8c479a6
rakpcodes <rakpcodes@gmail.com>
2022-06-16 17:09:11 -0400

    moved eventListener callback to separate function

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 
-window.addEventListener('keydown', function (e) {
+const playsound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(!audio) return; //stops the function from running when a keycode is pressed that isnt associated with an audio tag.
     audio.currentTime = 0; // returns the audio to the start on button press
     audio.play();
     key.classList.add('playing');
-})
 
-function removeTransition (e) {
-    if (e.propertyName !== 'transform') return // skips removing transition if it's not a transform
+}
+
+
+function removeTransition () {
     this.classList.remove('playing') //this refers to 'key' as it is the owner of the removeTransition function when it is called in the forEach
-
+    
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playsound)
