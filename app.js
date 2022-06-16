8d383fb82645edc5ab4c057e7da6cd2c2e6b58e7
rakpcodes <rakpcodes@gmail.com>
2022-06-16 16:50:55 -0400

    added removeTransition function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 
-
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(!audio) return; //stops the function from running when a keycode is pressed that isnt associated with an audio tag.
     audio.currentTime = 0; // returns the audio to the start on button press
-    audio.play()
+    audio.play();
+    key.classList.add('playing');
 })
+
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return // skips removing transition if it's not a transform
+    this.classList.remove('playing') //this refers to 'key' as it is the owner of the removeTransition function when it is called in the forEach
+
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
