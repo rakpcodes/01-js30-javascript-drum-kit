7d69b98b82cd5f6df17cbfa0ae9e9878e6921bff
rakpcodes <rakpcodes@gmail.com>
2022-06-16 15:51:50 -0400

    connected audio to keyCode

--- app.js
+++ app.js
@@ -2,5 +2,8 @@
 
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(audio)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return; //stops the function from running when a keycode is pressed that isnt associated with an audio tag.
+    audio.currentTime = 0; // returns the audio to the start on button press
+    audio.play()
 })
